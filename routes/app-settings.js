6bbfd7573c5a64468f940bf7411a8b1521cf8edf
Javier Arancibia <javier.arancibia@simpliciti.fr>
2020-01-24 16:20:08 +0100

    init

---
+++ routes/app-settings.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-mongoose');
+const { app_settings } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('app_settings');
+
+// This file contains the logic of every route in Forest Admin for the collection app_settings:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a App Setting
+router.post('/app_settings', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a App Setting
+router.put('/app_settings/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a App Setting
+router.delete('/app_settings/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of App Settings
+router.get('/app_settings', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of App Settings
+router.get('/app_settings/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a App Setting
+router.get('/app_settings/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of App Settings
+router.get('/app_settings.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
---
+++ routes/matchs.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-mongoose');
+const { matchs } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('matchs');
+
+// This file contains the logic of every route in Forest Admin for the collection matchs:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Match
+router.post('/matchs', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Match
+router.put('/matchs/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Match
+router.delete('/matchs/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Matchs
+router.get('/matchs', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Matchs
+router.get('/matchs/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Match
+router.get('/matchs/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Matchs
+router.get('/matchs.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
